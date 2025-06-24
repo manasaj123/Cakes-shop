1750f9ecfd6ca67eba295e41ac6fb21beabab690
manasaj123 <manasajangili99@gmail.com>
2025-06-24 10:17:55 +0530

    Added login feature to React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import cakes from './data';
+import CakeCard from './CakeCard';
+import './index.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
+
+  const filteredCakes = cakes.filter(cake =>
+    cake.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={`app ${darkMode ? 'dark-mode' : ''} animated-background`}>
+      
+
+      <header className="header">
+        <div className="top-header">
+          <div className="logo">🧁 Layers of Delight</div>
+          <div className="contact">Priya📞Contact Us: <a href="tel:9962607717">9962607717</a></div>
+        </div>
+
+        <div className="search-mode-container">
+          <input
+            type="text"
+            placeholder="Search cakes...🔍"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="search-input"
+          />
+          <button onClick={() => setDarkMode(!darkMode)} className="mode-toggle">
+            {darkMode ? '☀️ Light Mode' : '🌙 Dark Mode'}
+          </button>
+        </div>
       </header>
+
+      <main>
+        <h1 className="heading">Our Cakes & Cookies Menu</h1>
+        <div className="cake-grid">
+          {filteredCakes.map((cake, index) => (
+            <CakeCard cake={cake} key={index} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="footer">
+        <div> Order Contact:</div>
+        <div>© D.Shanmuga Priya, 8-2/34A Kamarajar Nagar 1st Street, Aundipatti - 625 512. Theni (Dist).</div>
+        <div className="footer-contact">Priya :📞 Contact Us: <a href="tel:9962607717">9962607717</a></div>
+      </footer>
     </div>
   );
 }
